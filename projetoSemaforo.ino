acb8ef1c36bbc4ef70d2954cc649b3ea03017bd5
retr0lbb <85702153+retr0lbb@users.noreply.github.com>
2022-04-11 15:27:23 -0300

    Add files via upload

---
+++ projetoSemaforo.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(13, LOW);
+  digitalWrite(11, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(12, LOW);
+}
+
